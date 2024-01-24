7d73a798f4168fe3d5a85fbe2829cd1fe3a8d7fc
sohaibkaouam <sohaib.kaouam@gmail.com>
2024-01-25 00:44:20 +0100

    get all films

--- src/main/java/com/film/repositories/FilmRepository.java
+++ src/main/java/com/film/repositories/FilmRepository.java
@@ -2,7 +2,9 @@ package com.film.repositories;
 
 import com.film.entities.Film;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FilmRepository extends JpaRepository<Film,Long> {
 
 }
